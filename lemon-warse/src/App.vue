50431a012b6ad0d5e31c0dee9821372e47b8cddf
kizya007 <kizya007@mail.ru>
2023-08-07 12:48:17 +0200

    create header

--- lemon-warse/src/App.vue
+++ lemon-warse/src/App.vue
@@ -1,17 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <lemonHeader />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import lemonHeader from "./components/lemonHeader.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    lemonHeader,
+  },
+};
 </script>
 
 <style>
