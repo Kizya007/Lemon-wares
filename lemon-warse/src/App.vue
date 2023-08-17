a55f514bd550a893076928cac4edf3b22bb0deb4
kizya007 <kizya007@mail.ru>
2023-08-17 11:58:31 +0200

    create footer and adaptive

--- lemon-warse/src/App.vue
+++ lemon-warse/src/App.vue
@@ -1,25 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <FooterInfo />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FooterInfo from "./components/FooterInfo.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    FooterInfo,
+  },
+};
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Poppins:wght@300;400;500;600;700&display=swap");
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
---
+++ lemon-warse/src/components/FooterInfo.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="footer">
+    <div class="left-block">
+      <div class="left-block__img">
+        <img src="../assets/img/WhiteLogo.png" alt="" />
+      </div>
+      <p class="left-block__text">
+        Lorem ipsum dolor sit amet, <br />
+        consectetur adipiscing elit ut aliquam, <br />
+        purus sit amet luctus venenatis, lectus <br />
+        magna fringilla urna, porttitor rhoncus <br />
+        dolor purus non enim praesent elem
+      </p>
+      <p class="left-block__copyright">
+        Built by Jeremiah with <HeartsIcon /> love in Lagos. Copyright 2021
+      </p>
+    </div>
+    <div class="footer-lists">
+      <ul class="footer-list">
+        <li class="footer-list__title"><a href="">Service</a></li>
+        <li class="footer-list__item"><a href="">Domain</a></li>
+        <li class="footer-list__item"><a href="">Hosting</a></li>
+        <li class="footer-list__item"><a href="">Cloud Hosting</a></li>
+        <li class="footer-list__item"><a href="">Private Hosting</a></li>
+      </ul>
+      <ul class="footer-list">
+        <li class="footer-list__title"><a href="">Hosting</a></li>
+        <li class="footer-list__item"><a href="">Cheap Hosting</a></li>
+        <li class="footer-list__item"><a href="">Hosting Wordpress</a></li>
+        <li class="footer-list__item"><a href="">Email Hosting</a></li>
+        <li class="footer-list__item"><a href="">Hosting Unlimited</a></li>
+      </ul>
+      <ul class="footer-list">
+        <li class="footer-list__title"><a href="">Company</a></li>
+        <li class="footer-list__item"><a href="">About</a></li>
+        <li class="footer-list__item"><a href="">Career</a></li>
+        <li class="footer-list__item"><a href="">Contact Us</a></li>
+      </ul>
+      <ul class="footer-list">
+        <li class="footer-list__title"><a href="">Help</a></li>
+        <li class="footer-list__item"><a href="">FAQ</a></li>
+        <li class="footer-list__item"><a href="">Help support</a></li>
+        
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import HeartsIcon from "../assets/icon/HeartsIcon.vue";
+
+export default {
+  components: {
+    HeartsIcon,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/Footer.scss";
+</style>
